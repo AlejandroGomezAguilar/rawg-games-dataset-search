bdb9e9cff913989d0b7b588dba73d43a9d01ec50
AlejandroGomezAguilar <118846578+AlejandroGomezAguilar@users.noreply.github.com>
2026-05-10 22:17:43 -0500

    Uncomment and include necessary header files

--- interface.c
+++ interface.c
@@ -4,12 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-//#include <sys/types.h>
-//#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-//#include <errno.h>
 #include <ctype.h>
-//#include <math.h>
 #include "common.h"
 
 // ===== UTILS TERMINAL ===== //
